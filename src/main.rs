96cb8f0d5cca91c39d2f99387c2c9e1d0aa39017
Neno Ganchev <neno.ganchev@gmail.com>
2022-12-18 22:43:15 +0200

    Change general arrangement of source files

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::env;
+
+mod day01;
+
+fn main() {
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.len() < 3 {
+        println!("Error: Missing arguments");
+        return;
+    }
+    let day = &args[0];
+    let part = &args[1];
+    let filename = &args[2];
+
+    match (day.as_str(), part.as_str()) {
+        ("day1", "part1") => day01::part1::solve(filename),
+        ("day1", "part2") => day01::part2::solve(filename),
+        _ => println!("Error: specify which part to solve in first argument"),
+    }
+}
